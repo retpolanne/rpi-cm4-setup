aa178c154e2c8343f5bb3fd058149a7169352511
Anne Isabelle "Anya" Macedo <annie@retpolanne.com>
2025-09-26 22:49:23 -0300

    No need for sonarr radarr anymore
    Signed-off-by: Anne Isabelle "Anya" Macedo <annie@retpolanne.com>

--- set-jellyfin-cpuset.sh
+++ set-jellyfin-cpuset.sh
@@ -4,11 +4,3 @@ container_id=$(docker ps -a | grep jellyfin | awk '{print $1}')
 container_name=$(docker ps -a | grep -Po 'addon_.*jellyfin')
 
 docker update --cpuset-cpus "1" $container_name
-
-container_name=$(docker ps -a | grep -Po 'addon_.*radarr')
-
-docker update --cpuset-cpus "1" $container_name
-
-container_name=$(docker ps -a | grep -Po 'addon_.*sonarr')
-
-docker update --cpuset-cpus "1" $container_name
